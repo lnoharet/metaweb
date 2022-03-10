b46c9a24da36ea2e8d3c5915ecdc1d55925702ee
Lisa Balzar <lisamariebalzar@gmail.com>
2022-03-10 13:59:30 +0100

    a lot of changes

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const db = mysql.createConnection({
   user:  "u11034_Ud000L22lA",//"u11034_FZqElbbcdW",
   password: "G=AmR.jw3OKwOeay9^FtSdSk",//"17RqL6+plrEBGp0t7djf==rL",
   database: "s11034_Dynmap"//"s11034_Analytics",*/
-  user:  "u11034_FZqElbbcdW",
+  user: "u11034_FZqElbbcdW",
   password: "17RqL6+plrEBGp0t7djf==rL",
   database: "s11034_Analytics"
 });
@@ -36,28 +36,28 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 
 
-  socket.on("get_users", function(){
+  socket.on("get_users", function () {
     console.log("fetching user list");
     const sql_query = "select uuid, name from plan_users";
-    db.query(sql_query, function(err, res){
+    db.query(sql_query, function (err, res) {
       if (err) throw err;
       io.sockets.emit("get_users_response", res);
     });
   });
 
-  socket.on("get_stat", function(arg){
+  socket.on("get_stat", function (arg) {
     var userid = arg.user;
     var stat = arg.stat;
     console.log("fetching stat for user ".concat(userid));
     console.log("fetching stat ".concat(stat));
 
-    switch(stat){
+    switch (stat) {
       case "1":
         // SQL query
         // Mob Kills
         const sql_query = (SQL`select (sum(case uuid when ${userid} then mob_kills else 0 end)) from plan_sessions`);
         console.log(sql_query);
-        db.query(sql_query, function(err, res){
+        db.query(sql_query, function (err, res) {
           if (err) throw err;
           console.log(res);
           io.sockets.emit("get_stat_response", res);
@@ -65,6 +65,7 @@ io.on("connection", (socket) => {
         break;
       case "2":
         // Player Kills
+
         break;
       case "3":
         // Deaths
@@ -83,7 +84,7 @@ io.on("connection", (socket) => {
     }
 
   });
-  
+
 });
 
 server.listen(process.env.PORT || 3000, () => {
