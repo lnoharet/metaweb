379bb0e4bbbdd04003cfc7fd77c6ef3046055f11
Lisa Balzar <lisamariebalzar@gmail.com>
2022-03-11 21:16:25 +0100

    adds ternary to handle data with all zeros

--- public/chart.js
+++ public/chart.js
@@ -2,8 +2,9 @@ function renderChart(data) {
   //const data = filtered_data; //[40, 39, 90, 100, 20];
   d3.select("svg").remove();
   console.log(data);
-  data_max = Math.max(...data);
-
+  const isAllZero = data.every(item => item === 0);
+  isAllZero ? data_max = 10 : data_max = Math.max(...data);
+  
   const w = 300;
   const h = 300;
   const svg = d3
@@ -65,4 +66,51 @@ function renderChart(data) {
       tooltip.html(``).style("visibility", "hidden");
     });
 
+}
+
+function renderNoData(data) {
+    const w = 300;
+    const h = 300;
+    const svg = d3
+        .select("#d3-container")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .style("overflow", "visible")
+        .style("padding-top", "40px")
+        .style("margin-left", "60px");
+
+    const xScale = d3
+        .scaleBand()
+        .domain(data.map((val, i) => i))
+        .range([0, w])
+        .padding(0.1);
+    
+    const yScale = d3.scaleLinear().domain([0, 10]).range([h, 0]);
+
+    const xAxis = d3.axisBottom(xScale).ticks(data.length);
+    
+    const yAxis = d3.axisLeft(yScale).ticks(7);
+
+    const tooltip = d3.select("body")
+        .append("div")
+        .attr("class","d3-tooltip")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .style("padding", "10px")
+        .style("background", "rgba(0,0,0,0)")
+        .style("border-radius", "5px")
+        .style("color", "#fff")
+        .style("font-family", 'Montserrat')
+        .style("font-size", '12px')
+        .text("a simple tooltip");
+
+  svg.append("g").call(xAxis).attr("transform", `translate(0, ${h})`).style("font-family", 'Montserrat');
+  svg.append("g").call(yAxis).style("font-family", 'Montserrat');
+    
+    svg.append("text")
+        .text("No Data")
+        .style("font-size", "40px")
+        .style("fill", "#fff");
 }
