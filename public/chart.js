6b50e9f0a6b4b6f5e4053c7eaaf5526c09db9364
Léo <leonoharet@n135-p130.eduroam.kth.se>
2022-03-11 14:07:54 +0100

    add stats to chart

--- public/chart.js
+++ public/chart.js
@@ -1,41 +1,39 @@
-const data = [40, 39, 90, 100, 20];
+function renderChart(data) {
+  //const data = filtered_data; //[40, 39, 90, 100, 20];
+  console.log(data);
+  const w = 300;
+  const h = 300;
+  const svg = d3
+    .select("#d3-container")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .style("overflow", "visible")
+    .style("padding-top", "40px")
+    .style("margin-left", "60px");
 
-const w = 300;
-const h = 300;
-const svg = d3.select('#d3-container')
-    .append('svg')
-    .attr('width', w)
-    .attr('height', h)
-    .style('overflow', 'visible')
-    .style('padding-top', '40px')
-    .style('margin-left', '60px')
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map((val, i) => i))
+    .range([0, w])
+    .padding(0.1);
 
-    const xScale = d3.scaleBand()
-        .domain(data.map((val, i) => i))
-        .range([0,w])
-        .padding(0.1);
-        
-    const yScale = d3.scaleLinear()
-        .domain([0,h])
-        .range([h, 0]);
+  const yScale = d3.scaleLinear().domain([0, h]).range([h, 0]);
 
-const xAxis = d3.axisBottom(xScale)
-    .ticks(data.length);
-    
-const yAxis = d3.axisLeft(yScale)
-    .ticks(7);
-    
-svg.append('g')
-    .call(xAxis)
-    .attr('transform', `translate(0, ${h})`)
-svg.append('g')
-    .call(yAxis);
+  const xAxis = d3.axisBottom(xScale).ticks(data.length);
 
-svg.selectAll('.bar')
+  const yAxis = d3.axisLeft(yScale).ticks(7);
+
+  svg.append("g").call(xAxis).attr("transform", `translate(0, ${h})`);
+  svg.append("g").call(yAxis);
+
+  svg
+    .selectAll(".bar")
     .data(data)
-    .join('rect')
-        .attr('x', (v, i) => xScale(i))
-        .attr('y', yScale)
-        .attr('width', xScale.bandwidth())
-        .attr('height', val => h - yScale(val))
-        .style('fill', '#B2085C');
+    .join("rect")
+    .attr("x", (v, i) => xScale(i))
+    .attr("y", yScale)
+    .attr("width", xScale.bandwidth())
+    .attr("height", (val) => h - yScale(val))
+    .style("fill", "#B2085C");
+}
